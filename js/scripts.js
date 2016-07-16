66c9b7602d32b49a8882ec2e58777007ad98b787
Charlie Hayward <c.hayward.mail@gmail.com>
2016-07-15 22:47:24 -0700

    Add more oop logic

--- js/scripts.js
+++ js/scripts.js
@@ -7,23 +7,21 @@ function Pizza(sizeCost, meatCost, vegetableCost, sauceCost) {
 
 Pizza.prototype.pizzaQuote = function () {
 
+
 }
 
 $(document).ready(function() {
   $("form#menu").submit(function(event) {
     event.preventDefault();
-    var ifSmall = $("input#sm").val();
-    var ifMedium = $("input#md").val();
-    var ifLarge = $("input#lg").val();
+
+    var sizeChoice = $("input.size").val();
     var meatChoice = $("input.meat").val();
     var vegChoice = $("input.vegetable").val();
     var sauceChoice = $("input.sauce").val();
 
-    var pizzaOrder = new Pizza(sizeChoice, meatChoice, vegChoice, ifSmall, ifMedium, ifLarge) {
-      
-    }
+    var pizzaOrder = new Pizza(sizeChoice, meatChoice, vegChoice, sauceChoice);
 
-
+    var finalPizza = pizzaOrder.pizzaQuote();
 
   });
 });
